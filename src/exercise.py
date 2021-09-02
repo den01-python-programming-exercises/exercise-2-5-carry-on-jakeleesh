2f501c26e2dbf08c3ceb6516e7684794ca9d1321
Jake Lee <jake.lee@fdmgroup.com>
2021-09-02 08:02:17 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    while True:
+        message = input("Carry on?")
+        if (message == "no"):
+            break
 
 if __name__ == '__main__':
     main()
